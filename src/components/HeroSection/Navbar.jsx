9b1718d98fff181305e56c577f99fcede06d69f2
jay9949 <jay37192@gmail.com>
2023-12-29 10:17:04 +0530

    #

--- src/components/HeroSection/Navbar.jsx
+++ src/components/HeroSection/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
